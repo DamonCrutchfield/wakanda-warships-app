51472ee56632d88dde75cea76f13c5cfd123ea61
ifeanchi <stanley.enyinnaya21@gmail.com>
2021-09-23 14:42:44 -0500

    updated the json data

--- public/react/components/About.js
+++ public/react/components/About.js
@@ -9,13 +9,9 @@ export const About = () => {
             </div>
             <blockquote className="blockquote text-center">
                 <p id="about-us" className="mb-0">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                    It has survived not only five centuries, but also the leap into electronic typesetting, 
-                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-                    sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus 
-                    PageMaker including versions of Lorem Ipsum.
+                There are three main aircraft in Wakanda: Talon Fighters, 
+                which are like fighter jets; Dragon Flyers, biomimetically designed helicopters that look like dragonflies; and the Royal Talon Fighter, T'Challa's version of Air Force One.
+
                 </p>
             </blockquote>
         </div>
