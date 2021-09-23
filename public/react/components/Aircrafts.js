50472d9471d18f16077be3ef3ea56bb7dcd6875b
DamonCrutchfield <crutchfielddamond@gmail.com>
2021-09-23 14:01:13 -0500

    Change semantics in app

--- public/react/components/Aircrafts.js
+++ public/react/components/Aircrafts.js
@@ -14,10 +14,10 @@ export const Aircrafts = (props) => {
                             <div style={{justifyContent : "center", textAlign: "center"}}>
                             <img class="card-img-top" style={{"width": "35rem"}} src={aircraft.image} alt="Card image cap"/>
                             <div class="card-body">
-                                <h5 class="card-title"><strong>Name: </strong>{aircraft.name}</h5>
+                                <p class="card-title"><strong>Name: </strong>{aircraft.name}</p>
                                 <p class="card-text"><strong>Description: </strong>{aircraft.description}</p>
                                 <p class="card-text"><strong>Role: </strong>{aircraft.role}</p>
-                                <p class="card-text"><strong>Type: </strong>{aircraft.type}</p>
+                                {/* <p class="card-text"><strong>Type: </strong>{aircraft.type}</p> */}
                                 <p class="card-text"><strong>Model: </strong>{aircraft.model}</p>
                             </div>
                         </div>
